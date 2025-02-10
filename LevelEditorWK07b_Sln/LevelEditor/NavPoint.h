408ee3914197d5aa4323d08ca40d9702b61f8027
CaineTinyArms <146839070+CaineTinyArms@users.noreply.github.com>
2025-02-10 16:25:11 +0000

    Added NavPoint implementation

--- LevelEditorWK07b_Sln/LevelEditor/NavPoint.h
+++ LevelEditorWK07b_Sln/LevelEditor/NavPoint.h
@@ -1,5 +1,24 @@
 #pragma once
+#include <glm/glm.hpp>
+#include <string>
 class NavPoint
 {
+public:
+	glm::vec3 pos;
+	glm::vec3 rot;
+	glm::vec3 scale;
+	std::string name;
+
+	int ID = -1;
+	bool open = false;
+	bool selected = false;
+
+	NavPoint(std::string _name)
+	{
+		name = _name;
+		pos = glm::vec3(0.0, 0.0, 0.0);
+		rot = glm::vec3(0.0, 0.0, 0.0);
+		scale = glm::vec3(1.0, 1.0, 1.0); 
+	}
 };
 
--- LevelEditorWK07b_Sln/LevelEditor/NavSet.h
+++ LevelEditorWK07b_Sln/LevelEditor/NavSet.h
@@ -1,5 +1,9 @@
 #pragma once
+#include<glm/glm.hpp>
+#include "Model.h"
 class NavSet
 {
+public:
+
 };
 
